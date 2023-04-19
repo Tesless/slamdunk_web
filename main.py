c7e56f3879b7367cce632b0b92a400e4a3a98062
tesless <lcm7644@gmail.com>
2023-04-19 13:03:15 +0900

    main 파일 이름 수정

--- main.py
+++ main.py
@@ -18,17 +18,22 @@ from starlette.templating import Jinja2Templates
 
 from src.schemas import Offer
 
+# 현재 스크립트 파일의 디렉토리 경로를 변수 ROOT에 저장합니다.
 ROOT = os.path.dirname(__file__)
 
+#FastAPI 애플리케이션을 생성합니다.
 app = FastAPI()
+#"/static" 경로에 정적 파일을 제공하기 위해 static 디렉토리를 마운트합니다.
 app.mount("/static", StaticFiles(directory="static"), name="static")
+#Jinja2 템플릿 엔진을 사용하여 "templates" 디렉토리에서 템플릿 파일을 로드합니다.
 templates = Jinja2Templates(directory="templates")
 
+#OpenCV의 CascadeClassifier를 사용하여 얼굴, 눈, 미소를 감지하는 분류기를 초기화합니다.
 faces = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
 eyes = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_eye.xml")
 smiles = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_smile.xml")
 
-
+#MediaStreamTrack 클래스를 상속하여 비디오 스트림을 변환하는 커스텀 비디오 스트림 트랙 클래스를 정의합니다.
 class VideoTransformTrack(MediaStreamTrack):
     """
     A video stream track that transforms frames from an another track.
@@ -79,35 +84,40 @@ class VideoTransformTrack(MediaStreamTrack):
             img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
 
             # rebuild a VideoFrame, preserving timing information
+            # 변환 작업을 수행한 후, VideoFrame.from_ndarray() 메서드를 사용하여 
+            # 변환된 이미지를 비디오 프레임으로 재구성하고, 타이밍 정보를 보존합니다. 이후, 재구성된 프레임을 반환합니다.
             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
             new_frame.pts = frame.pts
             new_frame.time_base = frame.time_base
             return new_frame
         elif self.transform == "rotate":
-            # rotate image
-            img = frame.to_ndarray(format="bgr24")
-            rows, cols, _ = img.shape
-            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-            img = cv2.warpAffine(img, M, (cols, rows))
+            # 이미지 회전
+            img = frame.to_ndarray(format="bgr24") # 현재 프레임을 ndarray 형태로 변환하여 img 변수에 저장
+            rows, cols, _ = img.shape # 이미지의 행, 열, 채널 정보를 가져와서 rows, cols 변수에 저장
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1) # 이미지의 중심을 중심으로 회전 변환 행렬 (Rotation Matrix) 생성
+            img = cv2.warpAffine(img, M, (cols, rows)) # 이미지에 회전 변환 적용
 
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
+            # VideoFrame 재구성, 타이밍 정보 보존
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24") # 회전된 이미지를 VideoFrame 형태로 변환하여 new_frame 변수에 저장
+            new_frame.pts = frame.pts # 기존 프레임의 pts (presentation timestamp) 정보를 new_frame의 pts로 복사
+            new_frame.time_base = frame.time_base # 기존 프레임의 time_base 정보를 new_frame의 time_base로 복사
+            return new_frame # 회전된 이미지를 반환
         elif self.transform == "cv":
-            img = frame.to_ndarray(format="bgr24")
-            face = faces.detectMultiScale(img, 1.1, 19)
-            for (x, y, w, h) in face:
+            # 얼굴 및 눈 검출
+            img = frame.to_ndarray(format="bgr24") # 현재 프레임을 ndarray 형태로 변환하여 img 변수에 저장
+            face = faces.detectMultiScale(img, 1.1, 19) # 얼굴 검출을 위해 detectMultiScale 함수를 사용하여 얼굴의 위치와 크기를 검출하여 face 변수에 저장
+            for (x, y, w, h) in face: # 검출된 얼굴의 위치와 크기를 가져와서 반복문을 통해 사각형으로 표시
                 cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            eye = eyes.detectMultiScale(img, 1.1, 19)
-            for (x, y, w, h) in eye:
+            eye = eyes.detectMultiScale(img, 1.1, 19) # 눈 검출을 위해 detectMultiScale 함수를 사용하여 눈의 위치와 크기를 검출하여 eye 변수에 저장
+            for (x, y, w, h) in eye: # 검출된 눈의 위치와 크기를 가져와서 반복문을 통해 사각형으로 표시
                 cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
+            # VideoFrame 재구성, 타이밍 정보 보존
+            new_frame = VideoFrame.from_ndarray(img, format="bgr24") # 검출된 얼굴과 눈이 표시된 이미지를 VideoFrame 형태로 변환하여 new_frame 변수에 저장
+            new_frame.pts = frame.pts # 기존 프레임의 pts (presentation timestamp) 정보를 new_frame의 pts로 복사
+            new_frame.time_base = frame.time_base # 기존 프레임의 time_base 정보를
+
             return new_frame
         else:
             return frame
@@ -121,22 +131,23 @@ def create_local_tracks(play_from=None):
         options = {"framerate": "30", "video_size": "640x480"}
         webcam = MediaPlayer("/dev/video1", format="v4l2", options=options)
             # audio, video = VideoTransformTrack(webcam.video, transform="cv")
+            ## 비디오 트랙에 대한 변환을 수행하는 코드
         relay = MediaRelay()
-        return None, relay.subscribe(webcam.video)
+        return None, relay.subscribe(webcam.video) ## 오디오는 None으로 반환하고, 비디오에 대해 미디어 릴레이를 구독하여 반환
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request}) ## 인덱스 페이지를 반환하는 코드
 
 
 @app.get("/cv", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index_cv.html", {"request": request})
+    return templates.TemplateResponse("index_cv.html", {"request": request}) # # OpenCV를 사용하는 인덱스 페이지를 반환하는 코드
 
 @app.get("/dashboard", response_class=HTMLResponse)
 async def dashboard(request: Request):
-    return templates.TemplateResponse("dashboard.html",{"request": request})
+    return templates.TemplateResponse("dashboard.html",{"request": request}) # 대시보드 페이지를 반환하는 코드
 
 @app.post("/offer")
 async def offer(params: Offer):
@@ -154,25 +165,25 @@ async def offer(params: Offer):
             pcs.discard(pc)
 
     # open media source
-    audio, video = create_local_tracks()
+    audio, video = create_local_tracks() # 로컬 미디어 트랙 생성
 
     # handle offer
-    await pc.setRemoteDescription(offer)
-    await recorder.start()
+    await pc.setRemoteDescription(offer) # 수신된 offer로 원격 설명 설정
+    await recorder.start() # 미디어 블랙홀로 녹음 시작
 
     # send answer
-    answer = await pc.createAnswer()
+    answer = await pc.createAnswer() # answer 생성
 
-    await pc.setRemoteDescription(offer)
+    await pc.setRemoteDescription(offer) # 다시 원격 설명 설정
     for t in pc.getTransceivers():
-        if t.kind == "audio" and audio:
+        if t.kind == "audio" and audio: # 오디오 트랙이 있을 경우 추가
             pc.addTrack(audio)
-        elif t.kind == "video" and video:
+        elif t.kind == "video" and video: # 비디오 트랙이 있을 경우 추가
             pc.addTrack(video)
 
-    await pc.setLocalDescription(answer)
+    await pc.setLocalDescription(answer) # 로컬 설명 설정
 
-    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type} # 생성된 로컬 설명을 반환하여 answer 전송
 
 
 @app.post("/offer_cv")
@@ -187,7 +198,7 @@ async def offer(params: Offer):
 
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
-        print("Connection state is %s" % pc.connectionState)
+        print("Connection state is %s" % pc.connectionState) # 연결 상태가 변할 때마다 출력
         if pc.connectionState == "failed":
             await pc.close()
             pcs.discard(pc)
@@ -199,25 +210,25 @@ async def offer(params: Offer):
         if track.kind == "video":
             pc.addTrack(
                 VideoTransformTrack(relay.subscribe(track), transform=params.video_transform)
-            )
+            ) # 비디오 트랙이 있을 경우, 변환된 트랙을 추가
             
         @track.on("ended")
         async def on_ended():
-            await recorder.stop()
+            await recorder.stop() # 트랙이 종료될 때 녹화 중지
 
     # handle offer
-    await pc.setRemoteDescription(offer)
-    await recorder.start()
+    await pc.setRemoteDescription(offer) # 수신된 offer로 원격 설명 설정
+    await recorder.start() # 미디어 블랙홀로 녹화 시작
 
     # send answer
-    answer = await pc.createAnswer()
-    await pc.setRemoteDescription(offer)
-    await pc.setLocalDescription(answer)
+    answer = await pc.createAnswer() # answer 생성
+    await pc.setRemoteDescription(offer) # 다시 원격 설명 설정
+    await pc.setLocalDescription(answer) # 로컬 설명 설정
 
-    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}  # 생성된 로컬 설명을 반환하여 answer 전송
 
 
-pcs = set()
+pcs = set() # 피어 커넥션 저장을 위한 집합
 args = ''
 
 
@@ -226,4 +237,5 @@ async def on_shutdown():
     # close peer connections
     coros = [pc.close() for pc in pcs]
     await asyncio.gather(*coros)
-    pcs.clear()
+    pcs.clear() # 앱 종료 시 피어 커넥션을 닫고 집합을 초기화
+
